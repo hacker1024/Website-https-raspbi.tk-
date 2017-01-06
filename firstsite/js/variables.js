068a536d57d4b64673c932d03594fa35c01d9daf
Josh Leivenzon <joshleivenzon@outlook.com>
2017-01-06 07:42:41 +0000

    synced

--- firstsite/js/variables.js
+++
@@ -1,6 +0,0 @@
-//This script makes it easy to customise elements across every webpage at once. (Thanks, jQuery!)
-
-//I can change settings easily by changing these variables.
-var websiteRoot = "https://raspbi.tk"
-var footer = "Created by superl2";
-var navbarCurrentPageBgColor = "#19192f"
